6cf426ca274485c780e1ece81c857fc854d6cc8d
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-10-04 01:15:46 +0200

    add frontend

---
+++ 01-login/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'contactbook.auth0.com',
+  clientId: 'hi17KwwiUis0KvVe4I50NnghqU2qOLiH',
+  callbackUrl: 'http://localhost:3000/callback'
+}
---
+++ web-contact-book/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'contactbook.auth0.com',
+  clientId: 'hi17KwwiUis0KvVe4I50NnghqU2qOLiH',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'https://contactbook'
+}
